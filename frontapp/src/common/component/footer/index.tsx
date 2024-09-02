80c31120b271ffd7951123f4e26b1ca67cd71f06
CanseFr <jucansell@gmail.com>
2024-09-02 15:03:23 +0200

    :recycle: Integration of dialog with legal text and upgrade list of social media

--- frontapp/src/common/component/footer/index.tsx
+++ frontapp/src/common/component/footer/index.tsx
@@ -1,41 +1,31 @@
 import Grid from "@mui/material/Grid2";
 import {Link} from "react-router-dom";
 import React from "react";
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import PinterestIcon from '@mui/icons-material/Pinterest';
-import XIcon from '@mui/icons-material/X';
-import YouTubeIcon from '@mui/icons-material/YouTube';
+import {legalItems, LegalProps} from "../../assets/legal-text";
+import {ScrollDialogChildText} from "../../dialog";
+import {socialMediaItems, SocialMediaProps} from "../../assets/social-media-links";
 
 export const Footer = () => (
     <Grid justifyContent="space-between" alignItems="center" textAlign="center" spacing={2} mt={10}>
         <Grid pl={3}>
             <Link to="/">
-                <img width={60} src={`${process.env.PUBLIC_URL}/assets/logo/logo-header.png`} alt="Favicon"/>
+                <img width={100} src={`${process.env.PUBLIC_URL}/assets/logo/logo-header.png`} alt="Favicon"/>
             </Link>
         </Grid>
 
-        <Grid container spacing={1} ml={2} mt={2} mb={5} justifyContent="center">
-            {[
-                <FacebookIcon/>,
-                <InstagramIcon/>,
-                <LinkedInIcon/>,
-                <PinterestIcon/>,
-                <XIcon/>,
-                <YouTubeIcon/>,
-            ].map((label) => (
+        <Grid container spacing={1} ml={2} mt={4} mb={5} justifyContent="center">
+            {socialMediaItems.map((media: SocialMediaProps) => (
                 <Grid>
-                    <Link style={{textDecoration: 'none', color: 'black'}} to="placeholder">{label}</Link>
+                    <Link style={{textDecoration: 'none', color: 'black'}} to={media.link}>{media.icon}</Link>
                 </Grid>
             ))}
         </Grid>
 
         <Grid container spacing={10} justifyContent="center">
-            {["Conditions générales d’utilisation", "Politique de confidentialité", "Mentions légales", "Gestion de la confidentialité"].map((label) => (
-                <Grid>
-                    <Link style={{textDecoration: 'none', color: 'black', fontSize: "0.8rem"}} to="placeholder">{label}</Link>
-                </Grid>
+            {legalItems.map((legal: LegalProps, index: number) => (
+                <ScrollDialogChildText key={index} title={legal.label}>
+                    {() => legal.children}
+                </ScrollDialogChildText>
             ))}
         </Grid>
     </Grid>
