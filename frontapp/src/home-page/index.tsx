7deb2b6a880d7b0f452bb1d48f36779403dea226
CanseFr <jucansell@gmail.com>
2024-09-02 11:59:06 +0200

    :recycle: Create bloc 3 home Page

--- frontapp/src/home-page/index.tsx
+++ frontapp/src/home-page/index.tsx
@@ -3,6 +3,9 @@ import {Typography} from "@mui/material";
 import {ButtonRounded} from "../common/component/buttons";
 import React from "react";
 import {InputsRoundedWithChild} from "../common/component/inputs";
+import TagIcon from '@mui/icons-material/Tag';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import EqualizerIcon from '@mui/icons-material/Equalizer';
 
 export const HomePage = () => {
     return (
@@ -64,6 +67,61 @@ export const HomePage = () => {
                         Un système point de vente click and collectpermettant aux clients d’effectuer leurs achats de façon flexible, en ligne ou en magasin. </Typography>
                     <ButtonRounded label="Découvrir"/>
                 </Grid>
+            </Grid>
+
+            <Grid
+                container
+                spacing={5}
+                alignItems="center"
+                pr={5}
+                pl={5}
+                pt={1}
+                pb={1}
+                mt={10}
+                justifyContent='center'
+                sx={{
+                    backgroundImage: `url(${process.env.PUBLIC_URL}/assets/illustrations/home-bg-3.png)`,
+                    backgroundSize: 'contain',
+                }}
+            >
+
+                <Grid width="35%" textAlign="left">
+                    <Grid container spacing={2} alignItems="center">
+                        <TagIcon/>
+                        <Typography variant="h5">
+                            Touchez de nouveaux client
+                        </Typography>
+                    </Grid>
+                    <Typography variant="subtitle2">
+                        Bla bla stat dash board ...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </Typography>
+
+                    <Grid container spacing={2} mt={2} alignItems="center">
+                        <AlternateEmailIcon/>
+                        <Typography variant="h5">
+                            Interagissez avec les clients
+                        </Typography>
+                    </Grid>
+                    <Typography variant="subtitle2" mb={2}>
+                        Bla bla stat dash board ...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </Typography>
+
+                    <Grid container spacing={2} mt={2} alignItems="center">
+                        <EqualizerIcon/>
+                        <Typography variant="h5">
+                            Exploitez les données
+                        </Typography>
+                    </Grid>
+                    <Typography variant="subtitle2">
+                        Bla bla stat dash board ...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </Typography>
+
+                </Grid>
+
+                <Grid>
+                    <img width={500} src={`${process.env.PUBLIC_URL}/assets/illustrations/home-3.png`} alt="Illustration"/>
+                </Grid>
+
             </Grid>
         </Grid>
     )
