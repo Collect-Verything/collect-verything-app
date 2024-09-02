0b5735256585b5b8f5925aa7026970c4f4c494f2
CanseFr <jucansell@gmail.com>
2024-09-02 16:26:25 +0200

    :sparkles: Create solution page

---
+++ frontapp/src/main-pages/solution/index.tsx
@@ -0,0 +1,38 @@
+import Grid from "@mui/material/Grid2";
+import {Typography} from "@mui/material";
+import {InputsRoundedWithChild, SwitchInputsRoundedWithChild} from "../../common/component/inputs";
+import {ButtonRounded} from "../../common/component/buttons";
+import React from "react";
+
+export const SolutionPage = () => {
+    return (
+        <Grid
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            textAlign="center"
+            spacing={2}
+            sx={{ width: '55%', margin: '0 auto' }}
+        >
+            <Typography mt={10} variant="h4" fontWeight={800}>
+                Démarrez gratuitement, puis profitez d’un tarif de 1 €/mois pendant 3 mois
+            </Typography>
+            <Typography mt={2} mb={2}  variant="subtitle2">
+                Essayez Shopify à moindre coût et sans risque pendant 3 mois.
+            </Typography>
+
+            <InputsRoundedWithChild >
+                <ButtonRounded label="Démarrer un essai" />
+            </InputsRoundedWithChild>
+
+            <Typography mt={2} mb={2} variant="subtitle2">
+                En saisissant votre e-mail, vous acceptez de recevoir des e-mails de marketing de la part de Shopify.
+            </Typography>
+
+            <SwitchInputsRoundedWithChild >
+                <ButtonRounded label="Démarrer un essai" />
+            </SwitchInputsRoundedWithChild>
+        </Grid>
+    );
+};
