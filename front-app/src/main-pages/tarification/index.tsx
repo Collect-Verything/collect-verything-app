599f68fe04348bcca3809e0699d03065e22da444
CanseFr <jucansell@gmail.com>
2024-09-18 11:00:13 +0200

    :arrow_up: Upgrade tarification component with link to baskt

--- front-app/src/main-pages/tarification/index.tsx
+++ front-app/src/main-pages/tarification/index.tsx
@@ -1,13 +1,16 @@
 import Grid from "@mui/material/Grid2";
 import { Typography } from "@mui/material";
 import React, { useState } from "react";
+import { SwitchInputsRoundedWithChild } from "../../common/component/inputs";
+import { ButtonRounded } from "../../common/component/buttons";
+import { Link } from "react-router-dom";
 
 export const Tarification = () => {
     const [selected, setSelected] = useState<number>(2);
 
     return (
         <Grid container mt={10} display="flex" flexDirection="column" alignContent="center" justifyContent="center">
-            <Grid container alignContent="center" justifyContent="center">
+            <Grid container alignContent="center" justifyContent="center" sx={{ "&:hover": { cursor: "pointer" } }}>
                 <Grid
                     width="33%"
                     height="30px"
@@ -59,13 +62,29 @@ export const Tarification = () => {
                 alignContent="center"
                 justifyContent="center"
             >
-                <Typography variant="subtitle2">1,8 % + 0,25 € EUR en ligne</Typography>
+                <Typography variant="h4" mt={3} mb={2}>
+                    Conçu pour l’extensibilité
+                </Typography>
+                <Typography variant="subtitle2" mb={3}>
+                    Évoluez rapidement avec notre boutique en ligne personnalisable, ou construisez la vôtre avec la
+                    technologie qui vous plaît pour une liberté créative totale. Mieux encore, intégrez vos systèmes
+                    tiers via les API de notre écosystème d’applications.
+                </Typography>
+
+                <SwitchInputsRoundedWithChild />
+
+                <Typography variant="subtitle2" mt={3}>
+                    1,8 % + 0,25 € EUR en ligne
+                </Typography>
                 <Typography variant="subtitle2">Le processus optimisé</Typography>
                 <Typography variant="subtitle2">Analyses de données standard</Typography>
                 <Typography variant="subtitle2">10 emplacements des stocks</Typography>
                 <Typography variant="subtitle2">Assistance par chat à tout moment</Typography>
                 <Typography variant="subtitle2">Vente globale localisée</Typography>
                 <Typography variant="subtitle2">Stockage serveur 2go</Typography>
+                <Link to={`/basket/${selected}`}>
+                    <ButtonRounded mt={3} mb={4} label={"Ajouter au panier"} />
+                </Link>
             </Grid>
         </Grid>
     );
