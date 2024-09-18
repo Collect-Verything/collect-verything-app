2f404dadfe35cf61f8ea56b178988ec602b11cc9
CanseFr <jucansell@gmail.com>
2024-09-18 16:30:03 +0200

    :children_crossing: Add button to consult basket

--- front-app/src/main-pages/tarification/index.tsx
+++ front-app/src/main-pages/tarification/index.tsx
@@ -1,15 +1,30 @@
 import Grid from "@mui/material/Grid2";
 import { Typography } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { PAID_FREQUENCY, SwitchRoundedWithPrice } from "../../common/component/inputs";
 import { ButtonRounded } from "../../common/component/buttons";
-import { Link } from "react-router-dom";
 import { poductsDetails } from "../../common/assets/products/poducts-details";
+import { Link } from "react-router-dom";
 
 export const Tarification = () => {
     const [selected, setSelected] = useState<number>(2);
     const [solutions, setSolutions] = useState(poductsDetails);
     const [paidFrequency, setPaidFrequency] = useState<PAID_FREQUENCY>(PAID_FREQUENCY.YEAR);
+    const [basketStorage, setBasketStorage] = useState<boolean>();
+
+    const handleSetStorage = (key: string, selected: number, paidFrequency: PAID_FREQUENCY) => {
+        setBasketStorage(true);
+        localStorage.setItem("basket", `${selected}-${paidFrequency}`);
+    };
+
+    useEffect(() => {
+        const userStorage = localStorage.getItem("basket");
+        if (userStorage) {
+            setBasketStorage(true);
+        } else {
+            setBasketStorage(false);
+        }
+    }, []);
 
     return (
         <Grid container mt={10} display="flex" flexDirection="column" alignContent="center" justifyContent="center">
@@ -83,9 +98,15 @@ export const Tarification = () => {
                         {detail}
                     </Typography>
                 ))}
-                <Link to={`/basket/${selected}/${paidFrequency}`}>
+                <Grid onClick={() => handleSetStorage("basket", selected, paidFrequency)}>
                     <ButtonRounded mt={3} mb={4} label={"Ajouter au panier"} />
-                </Link>
+                </Grid>
+
+                {basketStorage && (
+                    <Link to="/basket">
+                        <ButtonRounded mb={4} label={"Consulter votre panier"} bgColor="white" txtColor="black" />
+                    </Link>
+                )}
             </Grid>
         </Grid>
     );
