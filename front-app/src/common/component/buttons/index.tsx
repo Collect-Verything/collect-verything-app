85a25ed9cfa42aba4b4ceb8f86aeee1359f94161
CanseFr <jucansell@gmail.com>
2024-09-18 10:59:37 +0200

    :arrow_up: Upgrade ButtonRounded

--- front-app/src/common/component/buttons/index.tsx
+++ front-app/src/common/component/buttons/index.tsx
@@ -3,13 +3,17 @@ import { Button } from "@mui/material";
 
 interface ButtonRoundedProps {
     label: string;
+    mt?: number;
+    mb?: number;
 }
 
-export const ButtonRounded = ({ label }: ButtonRoundedProps) => {
+export const ButtonRounded = ({ label, mt = 0, mb = 0 }: ButtonRoundedProps) => {
     return (
         <Button
             variant="contained"
             sx={{
+                marginTop: mt,
+                marginBottom: mb,
                 bgcolor: "black",
                 color: "white",
                 padding: "5px 23px",
