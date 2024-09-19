2caa032481534ffe93dd7df8ab362e426b5c2fb2
CanseFr <jucansell@gmail.com>
2024-09-19 13:36:58 +0200

    :necktie: Add methode to get price and sanitize amount

--- front-app/src/common/utils/pricing/index.ts
+++ front-app/src/common/utils/pricing/index.ts
@@ -1,3 +1,15 @@
 export const mounthToAnnual = (price: number) => {
     return price * 10;
 };
+
+export const sanitizePrice = (price: number) => {
+    return price.toLocaleString("fr-FR", { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + " €";
+};
+
+export const getTva = (amount: number) => {
+    return amount * 0.2;
+};
+
+export const getHt = (amount: number) => {
+    return amount - amount * 0.2;
+};
