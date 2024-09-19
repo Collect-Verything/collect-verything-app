11944d0ef13519637e5ccba5961d95d1d0e25da8
CanseFr <jucansell@gmail.com>
2024-09-19 08:19:47 +0200

    :necktie: Manage basket price

--- front-app/src/main-pages/basket/index.tsx
+++ front-app/src/main-pages/basket/index.tsx
@@ -4,6 +4,8 @@ import { Button, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import { ListBasketType } from "../tarification";
 import { PAID_FREQUENCY } from "../../common/component/inputs";
+import CloseIcon from "@mui/icons-material/Close";
+import { mounthToAnnual } from "../../common/utils/pricing";
 
 interface BasketDetailsType {
     product: ProductsDetailsType;
@@ -13,6 +15,7 @@ interface BasketDetailsType {
 export const Basket = () => {
     const [listBasket, setListBasket] = useState<ListBasketType[]>([]);
     const [listProducts, setListProducts] = useState<BasketDetailsType[]>([]);
+    const [totalPrice, setTotalPrice] = useState(0);
 
     const loadBasketFromStorage = () => {
         const storedBasket = localStorage.getItem("basket");
@@ -45,6 +48,19 @@ export const Basket = () => {
         localStorage.setItem("basket", JSON.stringify(updatedBasket));
     };
 
+    // Calculer le prix total à chaque changement de listProducts
+    useEffect(() => {
+        const newTotal = listProducts.reduce((acc, prod) => {
+            const productPrice =
+                prod.frequency === PAID_FREQUENCY.YEAR
+                    ? mounthToAnnual(prod.product.price_mounth)
+                    : prod.product.price_mounth;
+            return acc + productPrice;
+        }, 0);
+        setTotalPrice(newTotal);
+    }, [listProducts]);
+
+    // Charger le panier depuis le localStorage une fois au chargement du composant
     useEffect(() => {
         loadBasketFromStorage();
     }, []);
@@ -53,14 +69,43 @@ export const Basket = () => {
         <Grid>
             {listProducts.length > 0 ? (
                 <Grid>
-                    {listProducts.map((product, index) => (
-                        <Grid key={index}>
-                            <Typography>{product.product.class}</Typography>
-                            <Typography>{product.frequency}</Typography>
-                            <Button onClick={() => handleDeleteProduct(index)}>Delete unit</Button>
+                    <Grid container justifyContent="center" spacing={2}>
+                        <Grid border={"2px solid #E7E6F6"} borderRadius="14px">
+                            {listProducts.map((product, index) => (
+                                <Grid container key={index} spacing={2} alignItems="center">
+                                    <img
+                                        width={60}
+                                        src={`${process.env.PUBLIC_URL}/assets/products/${product.product.picture_url}`}
+                                        alt="Favicon"
+                                    />
+
+                                    <Typography>{product.product.class}</Typography>
+                                    <Typography>{product.frequency}</Typography>
+                                    <Typography>
+                                        {product.frequency === PAID_FREQUENCY.MONTH
+                                            ? product.product.price_mounth
+                                            : mounthToAnnual(product.product.price_mounth)}{" "}
+                                        €
+                                    </Typography>
+
+                                    <Button onClick={() => handleDeleteProduct(index)}>
+                                        <CloseIcon />
+                                    </Button>
+                                </Grid>
+                            ))}
+                            <Button onClick={handleClearAll}>Clear All</Button>
                         </Grid>
-                    ))}
-                    <Button onClick={handleClearAll}>Clear all</Button>
+
+                        <Grid border={"2px solid #E7E6F6"} borderRadius="14px">
+                            <Typography>Total Ttc : {totalPrice}</Typography>
+                            {/*Creer methode pour avoir ht*/}
+                            <Typography> Ht : {totalPrice - totalPrice * 0.2}</Typography>
+                            {/*Creer methode pour avoir Tva*/}
+                            <Typography> Tva : {totalPrice * 0.2}</Typography>
+
+                            <Button>Paiement</Button>
+                        </Grid>
+                    </Grid>
                 </Grid>
             ) : (
                 <Typography>Votre panier est vide pour le moment</Typography>
