ccaa163c694e5319ca1dc1e7246cf12b920e504a
CanseFr <jucansell@gmail.com>
2024-09-20 22:46:02 +0200

    :rotating_light: Fix lint alert

--- front-app/src/tests/unit/sum.test.js
+++ front-app/src/tests/unit/sum.test.js
@@ -1,6 +1,8 @@
 function sum(a, b) {
     return a + b;
 }
+// eslint-disable-next-line no-undef
 test('adds 1 + 2 to equal 3', () => {
+    // eslint-disable-next-line no-undef
     expect(sum(1, 2)).toBe(3);
 });
