5c060d511c8af782e5b1c9dc6c965fdb6d261243
CanseFr <jucansell@gmail.com>
2024-09-23 16:37:22 +0200

    :seedling: Update database step by step, first role

--- auth-service/prisma/seed.ts
+++ auth-service/prisma/seed.ts
@@ -34,112 +34,112 @@ async function main() {
       name: 'SUPPORT',
     },
   });
-
-  const userSuperAdmin = await prisma.user.upsert({
-    where: { email: "admin@admin.admin" },
-    update: {},
-    create: {
-      firstname: 'Julien',
-      lastname: 'Cansell',
-      email: "admin@admin.admin",
-      password: "adminadmin",
-      birthDate: new Date('1992-06-10'),
-      gender: "MR",
-      phone: "0606060606",
-      roles: {
-        create: {
-          role: {
-            connect: { id: roleSuperAdmin.id },
-          },
-          assignedBy: "seed-setup",
-        },
-      },
-    },
-  });
-
-  const userSimple = await prisma.user.upsert({
-    where: { email: "user@user.user" },
-    update: {},
-    create: {
-      firstname: 'Brice',
-      lastname: 'Bite',
-      email: "user@user.user",
-      password: "useruser",
-      birthDate: new Date('1992-06-10'),
-      gender: "MR",
-      phone: "0606060606",
-      roles: {
-        create: {
-          role: {
-            connect: { id: roleUser.id },
-          },
-          assignedBy: "seed-setup",
-        },
-      },
-    },
-  });
-
-  const userInvoice = await prisma.user.upsert({
-    where: { email: "invoice@invoice.invoice" },
-    update: {},
-    create: {
-      firstname: 'Thomas',
-      lastname: 'Titoon',
-      email: "invoice@invoice.invoice",
-      password: "invoiceinvoice",
-      birthDate: new Date('1992-06-10'),
-      gender: "MR",
-      phone: "0606060606",
-      roles: {
-        create: {
-          role: {
-            connect: { id: roleInvoice.id },
-          },
-          assignedBy: "seed-setup",
-        },
-      },
-    },
-  });
-
-  const userInvoiceSupport = await prisma.user.upsert({
-    where: { email: "invoicesupport@invoicesupport.invoicesupport" },
-    update: {},
-    create: {
-      firstname: 'Isaac',
-      lastname: 'Lachnouff',
-      email: "invoicesupport@invoicesupport.invoicesupport",
-      password: "invoicesupport",
-      birthDate: new Date('1992-06-10'),
-      gender: "MR",
-      phone: "0606060606",
-      roles: {
-        create: [
-          {
-            role: {
-              connect: { id: roleInvoice.id },
-            },
-            assignedBy: "seed-setup",
-          },
-          {
-            role: {
-              connect: { id: roleSupport.id },
-            },
-            assignedBy: "seed-setup",
-          },
-        ],
-      },
-    },
-  });
+  //
+  // const userSuperAdmin = await prisma.user.upsert({
+  //   where: { email: "admin@admin.admin" },
+  //   update: {},
+  //   create: {
+  //     firstname: 'Julien',
+  //     lastname: 'Cansell',
+  //     email: "admin@admin.admin",
+  //     password: "adminadmin",
+  //     birthDate: new Date('1992-06-10'),
+  //     gender: "MR",
+  //     phone: "0606060606",
+  //     roles: {
+  //       create: {
+  //         role: {
+  //           connect: { id: roleSuperAdmin.id },
+  //         },
+  //         assignedBy: "seed-setup",
+  //       },
+  //     },
+  //   },
+  // });
+  //
+  // const userSimple = await prisma.user.upsert({
+  //   where: { email: "user@user.user" },
+  //   update: {},
+  //   create: {
+  //     firstname: 'Brice',
+  //     lastname: 'Bite',
+  //     email: "user@user.user",
+  //     password: "useruser",
+  //     birthDate: new Date('1992-06-10'),
+  //     gender: "MR",
+  //     phone: "0606060606",
+  //     roles: {
+  //       create: {
+  //         role: {
+  //           connect: { id: roleUser.id },
+  //         },
+  //         assignedBy: "seed-setup",
+  //       },
+  //     },
+  //   },
+  // });
+  //
+  // const userInvoice = await prisma.user.upsert({
+  //   where: { email: "invoice@invoice.invoice" },
+  //   update: {},
+  //   create: {
+  //     firstname: 'Thomas',
+  //     lastname: 'Titoon',
+  //     email: "invoice@invoice.invoice",
+  //     password: "invoiceinvoice",
+  //     birthDate: new Date('1992-06-10'),
+  //     gender: "MR",
+  //     phone: "0606060606",
+  //     roles: {
+  //       create: {
+  //         role: {
+  //           connect: { id: roleInvoice.id },
+  //         },
+  //         assignedBy: "seed-setup",
+  //       },
+  //     },
+  //   },
+  // });
+  //
+  // const userInvoiceSupport = await prisma.user.upsert({
+  //   where: { email: "invoicesupport@invoicesupport.invoicesupport" },
+  //   update: {},
+  //   create: {
+  //     firstname: 'Isaac',
+  //     lastname: 'Lachnouff',
+  //     email: "invoicesupport@invoicesupport.invoicesupport",
+  //     password: "invoicesupport",
+  //     birthDate: new Date('1992-06-10'),
+  //     gender: "MR",
+  //     phone: "0606060606",
+  //     roles: {
+  //       create: [
+  //         {
+  //           role: {
+  //             connect: { id: roleInvoice.id },
+  //           },
+  //           assignedBy: "seed-setup",
+  //         },
+  //         {
+  //           role: {
+  //             connect: { id: roleSupport.id },
+  //           },
+  //           assignedBy: "seed-setup",
+  //         },
+  //       ],
+  //     },
+  //   },
+  // });
 
   console.log({
     roleUser,
     roleSuperAdmin,
     roleInvoice,
     roleSupport,
-    userSuperAdmin,
-    userSimple,
-    userInvoice,
-    userInvoiceSupport,
+    // userSuperAdmin,
+    // userSimple,
+    // userInvoice,
+    // userInvoiceSupport,
   });
 }
 
