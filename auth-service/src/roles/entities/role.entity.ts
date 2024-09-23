3c89cbdbd4a230472c44f2c8b4985441dc672fe5
CanseFr <jucansell@gmail.com>
2024-09-23 17:30:52 +0200

    :sparkles: Generate user entity

--- auth-service/src/roles/entities/role.entity.ts
+++ auth-service/src/roles/entities/role.entity.ts
@@ -1,1 +1,16 @@
-export class Role {}
+import {Role} from "@prisma/client";
+import {ApiProperty} from "@nestjs/swagger";
+
+export class RoleEntity implements Role{
+    @ApiProperty()
+    id: number;
+
+    @ApiProperty()
+    name: string;
+
+    @ApiProperty()
+    createdAt: Date;
+
+    @ApiProperty()
+    updatedAt: Date;
+}
