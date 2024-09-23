f92bba09bd0621f055bf577e8b0c23d21aa814e7
CanseFr <jucansell@gmail.com>
2024-09-23 20:09:02 +0200

    :memo: Add todo info

--- auth-service/src/roles/roles.controller.ts
+++ auth-service/src/roles/roles.controller.ts
@@ -1,15 +1,28 @@
-import {Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, NotFoundException} from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
-import {ApiCreatedResponse, ApiOkResponse, ApiTags} from "@nestjs/swagger";
-import {RoleEntity} from "./entities/role.entity";
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { RoleEntity } from './entities/role.entity';
 
 @Controller('roles')
 @ApiTags('roles')
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
+  /*
+   * TODO: Aucun role ne peut etre crée, il faut donc le supprimer
+   * */
   @Post()
   @ApiCreatedResponse({ type: RoleEntity })
   create(@Body() createRoleDto: CreateRoleDto) {
@@ -22,6 +35,10 @@ export class RolesController {
     return this.rolesService.findAll();
   }
 
+  /*
+   * TODO: Peut etre necessaire de faire une selection d'un role pour savoir qui y est affecté, a voire
+   * */
+
   @Get(':id')
   @ApiOkResponse({ type: RoleEntity })
   async findOne(@Param('id') id: string) {
@@ -32,12 +49,21 @@ export class RolesController {
     return article;
   }
 
+  /*
+   * TODO: Un role ne peut pas etre modifié, il faut donc le supprimer
+   * */
   @Patch(':id')
   @ApiOkResponse({ type: RoleEntity })
-  update(@Param('id',ParseIntPipe) id: number, @Body() updateRoleDto: UpdateRoleDto) {
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateRoleDto: UpdateRoleDto,
+  ) {
     return this.rolesService.update(id, updateRoleDto);
   }
 
+  /*
+   * TODO: Un role ne peut pas etre supprimé, il faut donc le supprimer
+   * */
   @Delete(':id')
   @ApiOkResponse({ type: RoleEntity })
   remove(@Param('id', ParseIntPipe) id: number) {
