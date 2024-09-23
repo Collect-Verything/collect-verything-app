146f27b04288d721e1b3db13bb219000d5340ca0
CanseFr <jucansell@gmail.com>
2024-09-23 17:54:49 +0200

    :recycle: Upgrade role controllers

--- auth-service/src/roles/roles.controller.ts
+++ auth-service/src/roles/roles.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, NotFoundException} from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
@@ -24,19 +24,23 @@ export class RolesController {
 
   @Get(':id')
   @ApiOkResponse({ type: RoleEntity })
-  findOne(@Param('id') id: string) {
-    return this.rolesService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    const article = await this.rolesService.findOne(+id);
+    if (!article) {
+      throw new NotFoundException(`Role with ${id} does not exist.`);
+    }
+    return article;
   }
 
   @Patch(':id')
   @ApiOkResponse({ type: RoleEntity })
-  update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
-    return this.rolesService.update(+id, updateRoleDto);
+  update(@Param('id',ParseIntPipe) id: number, @Body() updateRoleDto: UpdateRoleDto) {
+    return this.rolesService.update(id, updateRoleDto);
   }
 
   @Delete(':id')
   @ApiOkResponse({ type: RoleEntity })
-  remove(@Param('id') id: string) {
-    return this.rolesService.remove(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.rolesService.remove(id);
   }
 }
