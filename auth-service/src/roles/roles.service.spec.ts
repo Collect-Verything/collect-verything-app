8107fb77f3ef65bf460f91e141b1be09fcf747f0
CanseFr <jucansell@gmail.com>
2024-09-24 11:09:53 +0200

    :white_check_mark: Fix test depending new services

--- auth-service/src/roles/roles.service.spec.ts
+++ auth-service/src/roles/roles.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RolesService } from './roles.service';
+import {PrismaModule} from "../prisma/prisma.module";
 
 describe('RolesService', () => {
   let service: RolesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       providers: [RolesService],
     }).compile();
 
