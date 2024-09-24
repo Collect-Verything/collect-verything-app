7e1392a7b5425c4893d6375911c99274b6b9350f
CanseFr <jucansell@gmail.com>
2024-09-24 18:53:26 +0200

    :rotating_light: Add comment to delete alert

--- front-app/src/common/tools/jwt/index.ts
+++ front-app/src/common/tools/jwt/index.ts
@@ -1,8 +1,10 @@
-import {jwtDecode} from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 
+// eslint-disable-next-line
 export const getDecodedAccessToken = (token: string): any => {
     try {
         return jwtDecode(token);
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (Error) {
         return null;
     }
