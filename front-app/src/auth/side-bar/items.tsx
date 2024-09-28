1d1b133144ea8b90cd38888074954c689e110a29
CanseFr <jucansell@gmail.com>
2024-09-28 20:42:31 +0200

    :sparkles: Create items menu super admin

--- front-app/src/auth/side-bar/items.tsx
+++ front-app/src/auth/side-bar/items.tsx
@@ -6,6 +6,9 @@ import SupportAgentIcon from "@mui/icons-material/SupportAgent";
 import ImportContactsIcon from "@mui/icons-material/ImportContacts";
 import React from "react";
 import { UserItemsDashboardType } from "./types";
+import ConnectWithoutContactIcon from "@mui/icons-material/ConnectWithoutContact";
+import Diversity2Icon from "@mui/icons-material/Diversity2";
+import SignalCellularAltIcon from "@mui/icons-material/SignalCellularAlt";
 
 export const UserItemsDashboard: UserItemsDashboardType[] = [
     {
@@ -27,6 +30,49 @@ export const UserItemsDashboard: UserItemsDashboardType[] = [
         icon: <DisplaySettingsIcon />,
         label: "Configuration solution",
         url: "config",
+    },
+    {
+        icon: <SupportAgentIcon />,
+        label: "Service client",
+        url: "support",
+    },
+    {
+        icon: <ImportContactsIcon />,
+        label: "Documentation",
+        url: "doc",
+    },
+];
+
+export const SuperAdminItemsDashboard: UserItemsDashboardType[] = [
+    {
+        icon: <DashboardIcon />,
+        label: "Tableau de bord",
+        url: "",
+    },
+    {
+        icon: <PersonIcon />,
+        label: "Compte",
+        url: "account",
+    },
+    {
+        icon: <PointOfSaleIcon />,
+        label: "Facturation",
+        url: "facturation",
+    },
+    {
+        icon: <Diversity2Icon />,
+        label: "Gestion du personnel",
+        url: "job",
+    },
+    {
+        icon: <ConnectWithoutContactIcon />,
+        label: "Gestion client",
+        url: "customer",
+    },
+    {
+        icon: <SignalCellularAltIcon />,
+        label: "Statistique",
+        url: "stats",
     },
     {
         icon: <SupportAgentIcon />,
