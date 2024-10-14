1e9056f3103497f6fad06ef704ae1b4ef6665d8e
CanseFr <jucansell@gmail.com>
2024-10-14 10:08:36 +0200

    :bug: Fix login request

--- front-app/src/shop/login/request.ts
+++ front-app/src/shop/login/request.ts
@@ -1,4 +1,4 @@
-import { api } from "../../common/utils/web";
+import { apiPost } from "../../common/utils/web";
 import { LoginProps } from "./index";
 
-export const loginRequest = (authLogin: LoginProps) => api(`auth/login/`, "POST", authLogin);
+export const loginRequest = (authLogin: LoginProps) => apiPost(`auth/login/`, "POST", authLogin);
