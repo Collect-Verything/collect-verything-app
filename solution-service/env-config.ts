d924cb77962c1f99922a49d8e369780a490f065c
CanseFr <jucansell@gmail.com>
2024-11-24 02:44:12 +0100

    :wrench: Fix port with a personalize port server

---
+++ solution-service/env-config.ts
@@ -0,0 +1,8 @@
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+
+export const configEnv = {
+  SOLUTION_PORT_API: process.env.SOLUTION_PORT,
+};
--- solution-service/src/main.ts
+++ solution-service/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {configEnv} from "../env-config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(configEnv.SOLUTION_PORT_API);
+
 }
 bootstrap();
