4b32da23974325c6f9578a53d746a94fc0eddfe6
CanseFr <jucansell@gmail.com>
2024-11-25 18:31:25 +0100

    :recycle: Add exception handeling

--- product-service/src/main.ts
+++ product-service/src/main.ts
@@ -1,8 +1,9 @@
-import { NestFactory } from '@nestjs/core';
+import {HttpAdapterHost, NestFactory} from '@nestjs/core';
 import { AppModule } from './app.module';
 import {configEnv} from "../env-config";
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 import { ValidationPipe } from '@nestjs/common';
+import {PrismaClientExceptionFilter} from "./prisma-client-exception/prisma-client-exception.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -23,6 +24,9 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
   await app.listen(configEnv.PRODUCT_PORT_API);
 }
