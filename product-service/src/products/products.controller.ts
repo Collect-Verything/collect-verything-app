58a1b55ceb2b0235061e2b3e12835884516e511c
CanseFr <jucansell@gmail.com>
2024-11-25 18:13:25 +0100

    :recycle: Upgrade entity

--- product-service/src/products/products.controller.ts
+++ product-service/src/products/products.controller.ts
@@ -2,7 +2,8 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import {ApiTags} from "@nestjs/swagger";
+import {ApiCreatedResponse, ApiTags} from "@nestjs/swagger";
+import {ProductEntity} from "./entities/product.entity";
 
 @Controller('products')
 @ApiTags('products')
@@ -10,31 +11,37 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: ProductEntity })
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Get()
+  @ApiCreatedResponse({ type: ProductEntity, isArray: true  })
   findAll() {
     return this.productsService.findAll();
   }
 
   @Get('non-visible')
+  @ApiCreatedResponse({ type: ProductEntity, isArray: true  })
   findAllNonVisible() {
     return this.productsService.findNonVisible();
   }
 
   @Get(':id')
+  @ApiCreatedResponse({ type: ProductEntity })
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiCreatedResponse({ type: ProductEntity })
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
 
   @Delete(':id')
+  @ApiCreatedResponse({ type: ProductEntity })
   remove(@Param('id') id: string) {
     return this.productsService.remove(+id);
   }
