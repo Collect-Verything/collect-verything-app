39726ed4f11ede377d181234386670fdfcdde7c0
CanseFr <jucansell@gmail.com>
2024-11-25 18:25:56 +0100

    :recycle: Add rules on product type and parse controller param

--- product-service/src/main.ts
+++ product-service/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {configEnv} from "../env-config";
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,8 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true,
   });
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   const config = new DocumentBuilder()
       .setTitle('Collect & Verything - Product')
