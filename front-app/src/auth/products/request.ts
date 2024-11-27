e56d732d15e7cfdb82668ac2446c4022a4b3819c
CanseFr <jucansell@gmail.com>
2024-11-27 18:49:09 +0100

    :sparkles: Create request to delete prod

---
+++ front-app/src/auth/products/request.ts
@@ -0,0 +1,11 @@
+import {apiDelete, apiPatch} from "../../common/utils/web";
+import { ProductUrlWithPort } from "../../app/micro-services";
+import {ProductEntity} from "../../shop/boutique/type";
+
+export const deleteProductById = (id: number) => {
+    return apiDelete(`${ProductUrlWithPort}/${id}`);
+};
+
+export const patchProductById = (id: number, product: ProductEntity) => {
+    return apiPatch(`${ProductUrlWithPort}/${id}`,product );
+};
