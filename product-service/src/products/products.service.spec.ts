0abccad499dc85f1241d006d4f974104d8586e4d
CanseFr <jucansell@gmail.com>
2024-11-28 14:05:36 +0100

    :white_check_mark: Fix test for ci

--- product-service/src/products/products.service.spec.ts
+++ product-service/src/products/products.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsService } from './products.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 describe('ProductsService', () => {
   let service: ProductsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       providers: [ProductsService],
     }).compile();
 
