24f7475c9e956f7393d5e1b1cc5e82e97dbeef79
CanseFr <jucansell@gmail.com>
2024-12-18 10:38:14 +0100

    :fire: Delete old placeholder for product

--- front-app/src/shop/basket/type.ts
+++
@@ -1,7 +0,0 @@
-import { ProductsDetailsType } from "../../common/assets/products/products-details";
-import { PAID_FREQUENCY } from "../boutique/const";
-
-export interface BasketDetailsType {
-    product: ProductsDetailsType;
-    frequency: PAID_FREQUENCY;
-}
