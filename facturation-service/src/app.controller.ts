42a99f7ceff3416f3dfd854f3c8c0a1a4ec59600
CanseFr <jucansell@gmail.com>
2025-01-22 18:47:57 +0100

    :recycle: Refactore stripe controllers and services

--- facturation-service/src/app.controller.ts
+++ facturation-service/src/app.controller.ts
@@ -1,37 +1,15 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  ParseIntPipe,
-  Patch,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Param, Patch, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ProductEntity } from './entities/product.entity';
 import { User } from './entities/user.entity';
-
-const stripe = require('stripe')('sk_test_VfGNimRoo2iCC7QIRyKnY3sc');
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  // INTENT
-  @Get(':amount')
-  async getIntent(@Param('amount', ParseIntPipe) amount: number) {
-    const intent = await stripe.paymentIntents.create({
-      amount,
-      currency: 'eur',
-      automatic_payment_methods: {
-        enabled: true,
-      },
-    });
-    return { client_secret: intent.client_secret };
-  }
-
-  // PRODUCT
+  // ----------
+  //  PRODUCT
+  // ----------
   @Post('create')
   ProductCreate(@Body() product: ProductEntity) {
     return this.appService.createProduct(product);
@@ -47,79 +25,41 @@ export class AppController {
     return this.appService.updateProduct(product);
   }
 
-  // CUSTOMER
+  // ----------
+  //  CUSTOMER
+  // ----------
   @Post('create-customer')
   async customerCreate(@Body() cleanedRegisterForm: User) {
-    const customer = await stripe.customers.create({
-      email: cleanedRegisterForm.email,
-      name: cleanedRegisterForm.lastname,
-      phone: cleanedRegisterForm.phone,
-      description: cleanedRegisterForm.id,
-      shipping: {
-        address: {
-          city: '',
-          country: '',
-          line1: '',
-          postal_code: '',
-          state: '',
-        },
-        name: cleanedRegisterForm.lastname,
-      },
-      address: {
-        city: '',
-        country: '',
-        line1: '',
-        postal_code: '',
-        state: '',
-      },
-    });
-    return customer;
+    return this.appService.createStripeCustomer(cleanedRegisterForm);
   }
 
-  //   SESSION BASKET
-
+  // ----------
+  //  SESSION BASKET
+  // ----------
   @Post('create-session/:id_stripe')
   async sessionCreate(
     @Param('id_stripe') id_stripe: string,
     @Body() basket: any,
   ) {
-    const listRaw = [];
-
-    const groupedItems = basket.reduce((acc, row) => {
-      const stripe_id_price = row.product.stripe_id_price;
-      if (!acc[stripe_id_price]) {
-        acc[stripe_id_price] = { price: stripe_id_price, quantity: 0 };
-      }
-      acc[stripe_id_price].quantity += 1;
-      return acc;
-    }, {});
-
-    for (const key in groupedItems) {
-      listRaw.push(groupedItems[key]);
-    }
-
-    const session = await stripe.checkout.sessions.create({
-      line_items: listRaw,
-      customer: id_stripe,
-      mode: 'subscription',
-      ui_mode: 'embedded',
-      return_url:
-        'http://localhost:3000/checkout/return?session_id={CHECKOUT_SESSION_ID}',
-    });
-
-    return { clientSecret: session.client_secret };
+    return this.appService.createStripeSession(id_stripe, basket);
   }
 
-  //   EVENT
-  // Dans un terminal:
-  // stripe login
-  // stripe listen --forward-to localhost:3003/event
 
-  // Dans un autre
-  // stripe trigger payment_intent.succeeded
+  // ----------
+  //  EVENT
+  // ----------
 
-  // Creer un module event
-  // creer une abse de donnée pour sotcker les lien des facture de chaque client ...
+  /*
+  * - Dans un terminal:
+  *     stripe login
+  *     stripe listen --forward-to localhost:3003/event
+  *
+  * - Dans un autre:
+  *     stripe trigger payment_intent.succeeded
+  *
+  * - Sinon copier coller les events de stripe et tester avec POSTMAN
+  */
+
   @Post('event')
   async eventGet(@Body() eventObject: any) {
     console.log(eventObject);
--- facturation-service/src/app.service.ts
+++ facturation-service/src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ProductEntity } from './entities/product.entity';
+import { User } from './entities/user.entity';
 
 const stripe = require('stripe')('sk_test_VfGNimRoo2iCC7QIRyKnY3sc');
 
@@ -23,6 +24,61 @@ export class AppService {
 
   deleteProduct(stripeProductId: string) {
     stripe.products.delete(stripeProductId);
+  }
+
+  async createStripeCustomer(cleanedRegisterForm: User) {
+    const customer = await stripe.customers.create({
+      email: cleanedRegisterForm.email,
+      name: cleanedRegisterForm.lastname,
+      phone: cleanedRegisterForm.phone,
+      description: cleanedRegisterForm.id,
+      shipping: {
+        address: {
+          city: '',
+          country: '',
+          line1: '',
+          postal_code: '',
+          state: '',
+        },
+        name: cleanedRegisterForm.lastname,
+      },
+      address: {
+        city: '',
+        country: '',
+        line1: '',
+        postal_code: '',
+        state: '',
+      },
+    });
+    return customer;
+  }
+
+  async createStripeSession(id_stripe: string, basket: any) {
+    const listRaw = [];
+
+    const groupedItems = basket.reduce((acc, row) => {
+      const stripe_id_price = row.product.stripe_id_price;
+      if (!acc[stripe_id_price]) {
+        acc[stripe_id_price] = { price: stripe_id_price, quantity: 0 };
+      }
+      acc[stripe_id_price].quantity += 1;
+      return acc;
+    }, {});
+
+    for (const key in groupedItems) {
+      listRaw.push(groupedItems[key]);
+    }
+
+    const session = await stripe.checkout.sessions.create({
+      line_items: listRaw,
+      customer: id_stripe,
+      mode: 'subscription',
+      ui_mode: 'embedded',
+      return_url:
+        'http://localhost:3000/checkout/return?session_id={CHECKOUT_SESSION_ID}',
+    });
+
+    return { clientSecret: session.client_secret };
   }
 
   /*
