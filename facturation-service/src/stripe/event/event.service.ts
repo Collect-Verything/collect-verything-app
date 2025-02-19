2657d4b7acf5c863b9263b6118b8a36e879344c5
CanseFr <jucansell@gmail.com>
2025-02-19 10:45:59 +0100

    :bug: Upgrade invoice for product

--- facturation-service/src/stripe/event/event.service.ts
+++ facturation-service/src/stripe/event/event.service.ts
@@ -1,23 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import {PrismaService} from "../../prisma/prisma.service";
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class StripeEventService {
-
-  constructor(
-      private prisma: PrismaService
-  ) {}
+  constructor(private prisma: PrismaService) {}
 
   async checkEvent(body: any) {
     if (body.object.object === 'invoice') {
       return await this.invoiceTreatment(body);
+    }
+    if (body.object.object === 'payment_intent') {
+      return await this.paymentIntentTreatment(body);
     } else {
-      console.log('autre');
+      console.log('Object recu non traité');
     }
   }
 
-  // payment_intent.succeeded
-  // Service
   async invoiceTreatment(invoice: any) {
     return this.prisma.facture.create({
       data: {
@@ -26,12 +24,25 @@ export class StripeEventService {
         customer: invoice.object.customer,
         currency: invoice.object.currency,
         invoice_link: invoice.object.hosted_invoice_url,
-        invoice_download:invoice.object.invoice_pdf,
-        start:invoice.object.lines.data[0].period.start,
-        end:invoice.object.lines.data[0].period.end,
+        invoice_download: invoice.object.invoice_pdf,
+        start: invoice.object.lines.data[0].period.start,
+        end: invoice.object.lines.data[0].period.end,
       },
-    })
-  // Product
-  //   ...
+    });
+  }
+
+  async paymentIntentTreatment(invoice: any) {
+    return this.prisma.facture.create({
+      data: {
+        id: invoice.object.id,
+        amount: invoice.object.amount,
+        customer: invoice.object.customer,
+        currency: invoice.object.currency,
+        invoice_link: invoice.object.charges.data[0].receipt_url,
+        invoice_download: invoice.object.charges.data[0].receipt_url,
+        start: invoice.object.created,
+        end: invoice.object.created,
+      },
+    });
   }
 }
