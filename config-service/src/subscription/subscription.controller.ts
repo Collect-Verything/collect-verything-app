5ab03be06eff8a61fc76811b12f3d95fbbf666c5
CanseFr <jucansell@gmail.com>
2025-02-20 11:27:59 +0100

    :sparkles: Setup base of get and persist subscription befor setup db

--- config-service/src/subscription/subscription.controller.ts
+++ config-service/src/subscription/subscription.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Param} from '@nestjs/common';
 import { SubscriptionService } from './subscription.service';
 
 @Controller('subscription')
 export class SubscriptionController {
   constructor(private readonly subscriptionService: SubscriptionService) {}
 
-  @Get()
-  findAll() {
-    return this.subscriptionService.findAll();
+  @Get(':user_stripe_id')
+  findAllByUserId(@Param('user_stripe_id') user_stripe_id: string) {
+    return this.subscriptionService.findAllByUserId(user_stripe_id);
   }
 }
--- config-service/src/subscription/subscription.service.ts
+++ config-service/src/subscription/subscription.service.ts
@@ -8,9 +8,19 @@ const stripe = require('stripe')('sk_test_VfGNimRoo2iCC7QIRyKnY3sc');
 
 @Injectable()
 export class SubscriptionService {
-  async findAll() {
-    return await stripe.subscriptions.list({
-      customer: "cus_RmzpxFcuDJqenR",
+  async findAllByUserId(user_stripe_id: string) {
+    const listSub = await stripe.subscriptions.list({
+      customer: user_stripe_id,
     });
+
+    await this.upsertSubscriptionsByUserId(user_stripe_id, listSub);
+
+    return listSub
+  }
+
+  async upsertSubscriptionsByUserId(user_stripe_id: string,listSub: any){
+    // map on listSub upsert sur listSub.id unique
+    console.log(user_stripe_id);
+    console.log(listSub);
   }
 }
