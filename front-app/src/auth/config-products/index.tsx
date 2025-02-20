739c1ceb557f4ddda6d86326814c6ec4c7853513
CanseFr <jucansell@gmail.com>
2025-02-20 14:32:59 +0100

    :label: Create type of clean subscription depending backend

--- front-app/src/auth/config-products/index.tsx
+++ front-app/src/auth/config-products/index.tsx
@@ -1,15 +1,16 @@
 import React, {useEffect, useState} from "react";
 import {apiGet} from "../../common/utils/web";
 import {useSelector} from "react-redux";
+import {SolutionSubscription} from "./type";
 
 // Pour recuperer des config de solution client il faut suivre le workflox definit dans la doc conernant la facturation des service, une fois ceci fait un  ou des subscription sont disponible et consultable
 
 export const ConfigProducts = () => {
     const user = useSelector((store: any) => store.authenticate);
-    const [listSolutions, setListSolutions] = useState<any[]>([]);
+    const [listSolutions, setListSolutions] = useState<SolutionSubscription[]>([]);
 
     useEffect(() => {
-        apiGet(`3004/subscription/${user.id_stripe}`).then((res)=>setListSolutions(res.data))
+        apiGet(`3004/subscription/${user.id_stripe}`).then(setListSolutions)
     }, []);
 
     if (listSolutions.length === 0) return <p>ConfigProducts page</p>;
---
+++ front-app/src/auth/config-products/type.ts
@@ -0,0 +1,12 @@
+export interface SolutionSubscription {
+    id: number;
+    user_stripe_id: string;
+    sub_stripe_id: string;
+    active_stripe: boolean;
+    published: boolean;
+    configured: boolean;
+    current_period_end: number;
+    current_period_start: number;
+    createdAt: string;
+    updatedAt: string;
+}
