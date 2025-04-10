c30212ae44dd06be8d9ab896c8212245ccbfadb7
CanseFr <jucansell@gmail.com>
2025-04-10 11:48:18 +0200

    :rocket: Init docker for mail service

--- mail-service/src/main.ts
+++ mail-service/src/main.ts
@@ -11,7 +11,6 @@ async function bootstrap() {
 bootstrap();
 /*
 TODO :
-[ ] Mise a jour des root dans Api Gateway
 
 
 [ ] Rajouter les script correspondant au besoin du nouveau service dans le package.json du root. (...)
@@ -22,17 +21,12 @@ TODO :
 ----[ ] Run
 
 
-[ ] Configuration du fichier main du nouveau service
-----[ ] Ajout du nouveau port dans le .env du root et
-----[ ] npm i dotenv dans le service concerné
-----[ ] Creer fichier env-config.ts pour la recuperation des .env du root (suivre pattern present dans les autres service)
-----[ ] Mettre à jour les .env front et root avec l'rl service et port du nouveau service
-
 
 [ ] Mettre a jour les fichier en rapport avec Docker (suivre pattern existant)
 ----[ ] Dockerfile
 ----[ ] compose.yaml
 ----[ ] start.sh
+
 [ ] Creer les tests unitaires
 [ ] Mettre a jour les action github en rajoutant le nouveau service, donc un nouveau fichier. (suivre le pattern)
 [ ] Mettre à jour les documentations : diagrammes de classes, de séquence, d’architecture, etc.
