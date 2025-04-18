20d1c72a427c92c91c6eed4e35269aa338b3d8d8
CanseFr <jucansell@gmail.com>
2025-04-18 15:35:50 +0200

    :recycle: Upgrade register validation

--- front-app/src/shop/register/index.tsx
+++ front-app/src/shop/register/index.tsx
@@ -1,8 +1,8 @@
 import Grid from "@mui/material/Grid2";
 import { TextField, Typography } from "@mui/material";
 import { ButtonRounded } from "../component/buttons";
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import dayjs from "dayjs";
@@ -23,9 +23,11 @@ import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import Alert from "@mui/material/Alert";
 import { registerRequest } from "./request";
+import DoneOutlineIcon from "@mui/icons-material/DoneOutline";
 
 // TODO : Les champs doivent etre vide a la validation du formulaire
 // TODO : Effectuer une redirection sur login apres avoir informé l'utilisateur que le register est confirmé
+
 /**
  * Composant/formulaire d'inscription utilisateur.
  *
@@ -51,8 +53,11 @@ import { registerRequest } from "./request";
  */
 
 export const RegisterPage = () => {
-    const [registerForm, setRegisterForm] = React.useState<UserRegisterType>(initRegisterForm);
-    const [alerts, setAlerts] = React.useState<AlertRegisterType | undefined>();
+    const [registerForm, setRegisterForm] = useState<UserRegisterType>(initRegisterForm);
+    const [alerts, setAlerts] = useState<AlertRegisterType | undefined>();
+    const [displaySuccess, setDisplaySuccess] = useState(false);
+
+    const nav = useNavigate();
 
     const handleRegister = async () => {
         const isValid = await checkRegisterForm(registerForm, setAlerts);
@@ -62,9 +67,11 @@ export const RegisterPage = () => {
         try {
             // eslint-disable-next-line @typescript-eslint/no-unused-vars
             const { confirmPassword, ...cleanedRegisterForm } = registerForm;
-            await registerRequest(cleanedRegisterForm);
-            setAlerts(ALERT_MESSAGE_FIELD.REGISTER_SUCCESS);
+            await registerRequest(cleanedRegisterForm).then(() => setDisplaySuccess(true));
             setRegisterForm(initRegisterForm);
+            setTimeout(() => {
+                nav(`/${URL_FRONT.LOGIN}`);
+            }, 2000);
         } catch {
             setAlerts(ALERT_MESSAGE_FIELD.REGISTER_FAILED);
         }
@@ -83,64 +90,100 @@ export const RegisterPage = () => {
                 </Link>
             </Grid>
 
-            <Grid pb={5} container flexDirection="column" justifyContent="center" alignItems="center">
-                {alerts && <Alert severity={alerts.alertStatus}>{alerts.alertMessage}</Alert>}
-                <Grid>
-                    <Typography pb={3} variant="h3" color="black">
-                        Welcome !
-                    </Typography>
-                </Grid>
-
-                {userRegisterList.map((field) => (
-                    <Grid key={field.key}>
-                        {field.key === "gender" && (
-                            <FormControl fullWidth sx={{ marginBottom: 2, borderRadius: "14px" }}>
-                                <Select
-                                    labelId="demo-simple-select-label"
-                                    id="demo-simple-select"
-                                    value={registerForm?.gender}
-                                    label="Civilité"
-                                    onChange={(e) => {
-                                        onChangeRegisterField("gender", setRegisterForm, e.target.value as string);
-                                    }}
-                                >
-                                    <MenuItem value="Monsieur">Monsieur</MenuItem>
-                                    <MenuItem value="Madame">Madame</MenuItem>
-                                </Select>
-                            </FormControl>
-                        )}
-
-                        {field.key === "birthDate" && (
-                            <DatePicker
-                                sx={{ marginBottom: 2, borderRadius: "14px" }}
-                                label={field.label}
-                                value={dayjs(registerForm.birthDate)}
-                                onChange={(e) => {
-                                    if (e) onChangeRegisterField(field.key, setRegisterForm, e.toISOString());
-                                }}
-                            />
-                        )}
-
-                        {field.key !== "birthDate" && field.key !== "gender" && (
-                            <TextField
-                                InputProps={{
-                                    style: { borderRadius: "25px", width: 300 },
-                                }}
-                                sx={{ paddingBottom: 2 }}
-                                label={field.label}
-                                onChange={(e) => onChangeRegisterField(field.key, setRegisterForm, e.target.value)}
-                                type={field.key === "password" || field.key === "confirmPassword" ? "password" : "text"}
-                                error={
-                                    field.key === "confirmPassword" &&
-                                    registerForm.password !== registerForm.confirmPassword
-                                }
-                            />
-                        )}
+            <Grid
+                pb={5}
+                container
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center"
+                textAlign="center"
+            >
+                {displaySuccess ? (
+                    <Grid
+                        bgcolor="white"
+                        spacing={5}
+                        borderRadius={5}
+                        margin={10}
+                        padding={10}
+                        boxShadow={5}
+                        border={1}
+                    >
+                        <Typography mb={2} variant="h3">
+                            Inscription confirmée
+                        </Typography>
+                        <DoneOutlineIcon color="success" />
                     </Grid>
-                ))}
-                <Grid mt={2}>
-                    <ButtonRounded label="Register" handleFx={handleRegister} />
-                </Grid>
+                ) : (
+                    <>
+                        {alerts && <Alert severity={alerts.alertStatus}>{alerts.alertMessage}</Alert>}
+                        <Grid>
+                            <Typography pb={3} variant="h3" color="black">
+                                Welcome !
+                            </Typography>
+                        </Grid>
+
+                        {userRegisterList.map((field) => (
+                            <Grid key={field.key}>
+                                {field.key === "gender" && (
+                                    <FormControl fullWidth sx={{ marginBottom: 2, borderRadius: "14px" }}>
+                                        <Select
+                                            labelId="demo-simple-select-label"
+                                            id="demo-simple-select"
+                                            value={registerForm?.gender}
+                                            label="Civilité"
+                                            onChange={(e) => {
+                                                onChangeRegisterField(
+                                                    "gender",
+                                                    setRegisterForm,
+                                                    e.target.value as string,
+                                                );
+                                            }}
+                                        >
+                                            <MenuItem value="Monsieur">Monsieur</MenuItem>
+                                            <MenuItem value="Madame">Madame</MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                )}
+
+                                {field.key === "birthDate" && (
+                                    <DatePicker
+                                        sx={{ marginBottom: 2, borderRadius: "14px" }}
+                                        label={field.label}
+                                        value={dayjs(registerForm.birthDate)}
+                                        onChange={(e) => {
+                                            if (e) onChangeRegisterField(field.key, setRegisterForm, e.toISOString());
+                                        }}
+                                    />
+                                )}
+
+                                {field.key !== "birthDate" && field.key !== "gender" && (
+                                    <TextField
+                                        InputProps={{
+                                            style: { borderRadius: "25px", width: 300 },
+                                        }}
+                                        sx={{ paddingBottom: 2 }}
+                                        label={field.label}
+                                        onChange={(e) =>
+                                            onChangeRegisterField(field.key, setRegisterForm, e.target.value)
+                                        }
+                                        type={
+                                            field.key === "password" || field.key === "confirmPassword"
+                                                ? "password"
+                                                : "text"
+                                        }
+                                        error={
+                                            field.key === "confirmPassword" &&
+                                            registerForm.password !== registerForm.confirmPassword
+                                        }
+                                    />
+                                )}
+                            </Grid>
+                        ))}
+                        <Grid mt={2}>
+                            <ButtonRounded label="Register" handleFx={handleRegister} />
+                        </Grid>
+                    </>
+                )}
             </Grid>
         </Grid>
     );
