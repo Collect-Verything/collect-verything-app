96673340fb728af8dfb6ff84fa0a5e40f10080b0
CanseFr <jucansell@gmail.com>
2025-04-18 19:59:03 +0200

    :rocket: Upgrade env api gateway

--- api-gateway/env-config.ts
+++ api-gateway/env-config.ts
@@ -1,8 +1,3 @@
-import * as dotenv from 'dotenv';
-import * as path from 'path';
-
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
-
 export const configEnv = {
   // MAIN DOMAIN
   DOMAIN: process.env.DOMAIN,
