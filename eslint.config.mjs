397299c87d0e1a05a9605657452ce370b057c966
CanseFr <jucansell@gmail.com>
2025-04-19 10:47:30 +0200

    :rotating_light: Try fix lint config and check git actions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,40 @@
-import globals from "globals";
+// eslint.config.mjs
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import tseslint from "typescript-eslint";
+import globals from "globals";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-    {
-        // "rules": { "react/no-unescaped-entities": 0 }
+  {
+    files: ["**/*.{js,ts,jsx,tsx}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
     },
+    plugins: {
+      react: pluginReact,
+      "@typescript-eslint": tseslint.plugin,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslint.configs.recommended[0].rules,
+      ...pluginReact.configs.recommended.rules,
+      "no-unused-vars": "warn",
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
 ];
