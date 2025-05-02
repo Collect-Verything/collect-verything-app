c66516eef0c2cd8d6f474ec399c33b5250108609
CanseFr <jucansell@gmail.com>
2025-05-02 15:42:18 +0200

    :lipstick: Upgrade no documentation

--- front-app/src/auth/documentation/index.tsx
+++ front-app/src/auth/documentation/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Box, Grid2, Paper, Typography } from "@mui/material";
+import ArticleIcon from "@mui/icons-material/Article";
 /*
  * Les composant documentation sont pour le moment commun à un user comme a un super admin ou metier concerné.
  * If user get all documentation
@@ -6,5 +8,34 @@ import React from "react";
  * Trouver un moyen de rendre commun le composant mais de changer le type de d'action dans le display du tableau selon le role detecté
  * */
 export const Documentation = () => {
-    return <p>Documentation page</p>;
+    return (
+        <Grid2
+            container
+            direction="column"
+            margin="auto"
+            justifyContent="center"
+            alignItems="center"
+            textAlign="center"
+            minHeight="100vh"
+        >
+            <Paper
+                elevation={3}
+                sx={{
+                    px: 6,
+                    py: 8,
+                    maxWidth: 420,
+                    textAlign: "center",
+                    borderRadius: 4,
+                }}
+            >
+                <Box display="flex" justifyContent="center" mb={2}>
+                    <ArticleIcon sx={{ fontSize: 56, color: "text.secondary" }} />
+                </Box>
+
+                <Typography variant="h6" gutterBottom>
+                    Aucune documentation pour l’instant
+                </Typography>
+            </Paper>
+        </Grid2>
+    );
 };
